fad5120748e0a5a686e2c040bb12b8364721f0f3
Jewelsnel <jewelle1@live.nl>
2019-03-04 17:42:23 +0100

    Added: eerste versie van Pizza paul. Player > Targets > Score

---
+++ eindopdracht_pizza_paul_game/Assets/Project/Scripts/Townspeople.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEngine;
+
+public class Townspeople : MonoBehaviour
+{
+
+    public Material[] material;
+    Renderer rend;
+    public Score score;
+
+    public float health = 50f;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+    }
+
+
+    public void TakeDamage (float amount)
+    {
+        health -= amount;
+        if (health == 0f)
+        {
+            FullStomache();
+            Score.scoreAmount += 1;
+
+        }
+
+    }
+
+    private void FullStomache ()
+    {
+        
+        rend.sharedMaterial = material[1];
+        
+
+
+    }
+}
