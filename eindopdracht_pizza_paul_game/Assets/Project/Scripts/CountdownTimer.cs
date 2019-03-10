abc3e86adecd33909514a8573f9ee86fa2a319e3
Jewelsnel <jewelle1@live.nl>
2019-03-10 15:13:30 +0100

    Added: Timer System, Score System, Enemy prefab, Win/Game over Level

---
+++ eindopdracht_pizza_paul_game/Assets/Project/Scripts/CountdownTimer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    public float startingTime = 10f;
+
+    public Text TimerText;
+    public GameManager gameManager;
+
+         
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    public void Update()
+    {
+        currentTime -= Time.deltaTime;
+
+        int seconds = (int)(currentTime % 60);
+        int minutes = (int) (currentTime / 60) %60;
+
+
+        string timerString = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        TimerText.text = timerString;
+
+
+        if (currentTime <= 0f)
+        {
+            gameManager.GameIsWon();
+        }
+
+
+    }
+
+
+
+
+}
+    
+
+
+   
+
---
+++ eindopdracht_pizza_paul_game/Assets/Project/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameIsWon = false;
+    bool gameHasEnded = false;
+
+    public float sceneLoadDelay = 1f;
+
+
+
+    public void GameIsWon()
+    {
+        if (gameIsWon == false)
+        {
+            gameIsWon = true;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        
+    }
+        
+
+
+    public void GameHasEnded()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+            
+        }
+
+
+
+
+    }
+            
+}
